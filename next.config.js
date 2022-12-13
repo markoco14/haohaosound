574599a00b2a37cf3233c762b2b3e4aec757273b
Mark O'Connor <mark.oconnor14@gmail.com>
2022-12-14 00:01:51 +0700

    fix gitignore and others

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
+  reactStrictMode: true,
+})
+
