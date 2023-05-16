7f1f965caf3030851ade8b2a098e7cd271fe75c0
Mark O'Connor <mark.oconnor14@gmail.com>
2023-05-16 12:25:30 +0800

    feat: change main page to favorite list

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,46 @@ import Head from 'next/head'
 import { useRef } from 'react'
 import Link from 'next/link'
 import Navbar from '../components/Navbar';
+import React from 'react';
+
+// add server side props 
 
 export default function Home() {
+
+  const soundLinks = [
+		{
+			action: "拍手",
+			link: "audio/clap_hands.mp3"
+		},
+		{
+			action: "歡呼",
+			link: "audio/crowd_cheer.mp3"
+		},
+		{
+			action: "嘘声",
+			link: "audio/crowd_boo.mp3"
+		},
+		{
+			action: "擊鼓",
+			link: "audio/joke_drum.mp3"
+		},
+		{
+			action: "蟋蟀",
+			link: "audio/crickets.wav"
+		},
+		{
+			action: "放屁",
+			link: "audio/fart.wav"
+		},
+	];
+
+
+  const elementRefs = useRef([]);
+
+	soundLinks.forEach((_, index) => {
+    elementRefs.current[index] = React.createRef();
+  });
+
 
   return (
     <div>
@@ -13,15 +51,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 flex flex-col text-white'>
-        <Navbar></Navbar>
-				<div className='p-2 mb-8'>
-					<h1 className='text-4xl'>好好Sound</h1>
-					<p>每時每刻的完美聲音</p>
-				</div>
-        <div className='px-2'>
-          <Link href="/library" className="bg-rose-500  p-4 rounded">試試看</Link>
-          {/* <Link href="signup">Create Account</Link> */}
-        </div>
+				<Navbar />
+				<section>
+					<article className='p-2'>
+						<h1 className="text-2xl">生日快樂皓皓</h1>
+					</article>
+					<ul className="p-2 flex flex-col gap-4">
+						{soundLinks.map((sound, index) => (
+							<li key={index}>
+								<button
+									className="w-full bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
+									onClick={() => {
+										elementRefs.current.forEach((ref, refIndex) => {
+											if (refIndex !== index) {
+												elementRefs.current[refIndex].current.pause()
+												elementRefs.current[refIndex].current.currentTime = 0
+											}
+										})
+										if (!elementRefs.current[index].current.paused) {
+											elementRefs.current[index].current.currentTime = 0;
+										}
+										elementRefs.current[index].current.play()
+									}}
+								>{sound.action}</button>
+								<audio ref={elementRefs.current[index]} src={sound.link}>
+									Your browser does not support the <code>audio</code> element.
+								</audio>
+							</li>
+						))}
+					</ul>
+				</section>
       </main>
     </div>
   )
