2eec8be872bd69bee8caa4147ad44b0f756bb3d4
Mark O'Connor <mark.oconnor14@gmail.com>
2023-05-23 10:11:01 +0800

    feat: implement create free list feature

--- pages/[list_id].tsx
+++ pages/[list_id].tsx
@@ -1,43 +1,77 @@
 import Head from 'next/head'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { useRouter } from 'next/router';
 
 export default function Library() {
+
+	const [sounds, setSounds] = useState([]);
 	
-	const soundLinks = [
-		{
-			action: "拍手",
-			link: "audio/clap_hands.mp3"
-		},
-		{
-			action: "歡呼",
-			link: "audio/crowd_cheer.mp3"
-		},
-		{
-			action: "嘘声",
-			link: "audio/crowd_boo.mp3"
-		},
-		{
-			action: "擊鼓",
-			link: "audio/joke_drum.mp3"
-		},
-		{
-			action: "蟋蟀",
-			link: "audio/crickets.wav"
-		},
-		{
-			action: "放屁",
-			link: "audio/fart.wav"
-		},
-	];
+	const router = useRouter();
+	console.log(router.query.list_id);
 
+	
+
+	
+		
+	// setSounds(soundLinks);
+	
+	useEffect(() => {
+		console.log('using use effect')
+		if (router.query.list_id === 'freelist') {
+			console.log(JSON.parse(localStorage.getItem("nonUserList")).sounds)
+			const soundLinks = JSON.parse(localStorage.getItem("nonUserList")).sounds
+			// const soundLinks = [
+			// 	{
+			// 		action: "蟋蟀",
+			// 		link: "audio/crickets.wav"
+			// 	},
+			// 	{
+			// 		action: "放屁",
+			// 		link: "audio/fart.wav"
+			// 	},
+			// ];
+			setSounds(soundLinks);
+			return
+		}
+
+		const soundLinks = [
+			{
+				action: "拍手",
+				link: "audio/clap_hands.mp3"
+			},
+			{
+				action: "歡呼",
+				link: "audio/crowd_cheer.mp3"
+			},
+			{
+				action: "嘘声",
+				link: "audio/crowd_boo.mp3"
+			},
+			{
+				action: "擊鼓",
+				link: "audio/joke_drum.mp3"
+			},
+			{
+				action: "蟋蟀",
+				link: "audio/crickets.wav"
+			},
+			{
+				action: "放屁",
+				link: "audio/fart.wav"
+			},
+		];
+		setSounds(soundLinks);
+	}, [router.query.list_id])
+	
+	
 	const elementRefs = useRef([]);
-
-	soundLinks.forEach((_, index) => {
-    elementRefs.current[index] = React.createRef();
-  });
+	sounds?.forEach((_, index) => {
+			elementRefs.current[index] = React.createRef();
+		});
+	// sounds.length > 0 ? console.log('greater') : console.log('not')
 
   return (
     <div>
@@ -54,7 +88,7 @@ export default function Library() {
 						<h1 className="text-2xl">生日快樂皓皓</h1>
 					</article>
 					<ul className="p-2 flex flex-col gap-4">
-						{soundLinks.map((sound, index) => (
+						{sounds?.map((sound, index) => (
 							<li key={index}>
 								<button
 									className="w-full bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
