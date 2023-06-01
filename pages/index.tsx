a71c91fef737674dc35b290617ef74a04e2df312
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-01 10:11:55 +0800

    add max width container to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,37 +44,39 @@ export default function Home({list}) {
         <meta name="description" content="Play the perfect sound for every moment." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 flex flex-col text-white'>
-				<Navbar />
-				<section>
-					<article className='p-2'>
-						<h1 className="text-2xl">{list.name}</h1>
-					</article>
-					<ul className="p-2 flex flex-col gap-4">
-						{sounds.map((sound, index) => (
-							<li key={index}>
-								<button
-									className="w-full bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
-									onClick={() => {
-										elementRefs.current.forEach((ref, refIndex) => {
-											if (refIndex !== index) {
-												elementRefs.current[refIndex].current.pause()
-												elementRefs.current[refIndex].current.currentTime = 0
+      <main className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800  ">
+        <div className="sm:max-w-[600px] text-white mx-auto">
+					<Navbar />
+					<section>
+						<article className='p-2'>
+							<h1 className="text-2xl">{list.name}</h1>
+						</article>
+						<ul className="p-2 flex flex-col gap-4">
+							{sounds.map((sound, index) => (
+								<li key={index}>
+									<button
+										className="w-full bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
+										onClick={() => {
+											elementRefs.current.forEach((ref, refIndex) => {
+												if (refIndex !== index) {
+													elementRefs.current[refIndex].current.pause()
+													elementRefs.current[refIndex].current.currentTime = 0
+												}
+											})
+											if (!elementRefs.current[index].current.paused) {
+												elementRefs.current[index].current.currentTime = 0;
 											}
-										})
-										if (!elementRefs.current[index].current.paused) {
-											elementRefs.current[index].current.currentTime = 0;
-										}
-										elementRefs.current[index].current.play()
-									}}
-								>{sound.name}</button>
-								<audio ref={elementRefs.current[index]} src={sound.audio_url}>
-									Your browser does not support the <code>audio</code> element.
-								</audio>
-							</li>
-						))}
-					</ul>
-				</section>
+											elementRefs.current[index].current.play()
+										}}
+									>{sound.name}</button>
+									<audio ref={elementRefs.current[index]} src={sound.audio_url}>
+										Your browser does not support the <code>audio</code> element.
+									</audio>
+								</li>
+							))}
+						</ul>
+					</section>
+				</div>
       </main>
     </div>
   )
--- pages/lists.tsx
+++ pages/lists.tsx
@@ -59,7 +59,7 @@ const EditListModal = ({localListElementRefs, selectedList, localList, setLocalL
     }, []);
 
     return (
-      <article className="p-4 absolute bottom-0 left-0 h-[90vh] w-full bg-slate-800 opacity-95 rounded-t-lg overflow-y-scroll">
+      <article className="bg-slate-800 p-4 rounded text-white sm:max-w-[600px] w-full">
         <h2>Edit &quot;{selectedList}&quot;</h2>
         {/* <p>List: {selectedList}</p> */}
         {/* <p>sounds</p> */}
@@ -141,7 +141,7 @@ const EditListModal = ({localListElementRefs, selectedList, localList, setLocalL
 
         {/* list of all sounds from db */}
         {loading ? (
-          <p>Loading...</p>
+          <p className="mx-auto w-full h-[250px] grid place-content-center">Loading...</p>
         ) : (
           <ul className="mb-4 overflow-y-auto grid grid-cols-2 gap-2 max-h-[400px]">
             {sounds?.map((sound, index) => (
@@ -215,17 +215,14 @@ const EditListModal = ({localListElementRefs, selectedList, localList, setLocalL
 
                     // IF LIST IS FULL, TELL USER
                     toast((t) => (
-                      <div>
-                        <p className="mb-4">
+                      <div className="z-100">
+                        <p>
                           This list is full. Please delete a sound to add new ones.
                         </p>
-                        <div className='flex justify-center'>
-                          <button onClick={() => toast.dismiss(t.id)}>
-                            Dismiss
-                          </button>
-                        </div>
                       </div>
-                    ));
+                    ), {
+                      duration: 1500,
+                    });
                   }}
                 >
                   <span className="material-symbols-outlined">add</span>
@@ -235,9 +232,9 @@ const EditListModal = ({localListElementRefs, selectedList, localList, setLocalL
           </ul>
         )}
 
-        <div className="absolute bottom-0 left-0 h-12 w-full flex justify-evenly">
+        <div className="h-12 w-full flex justify-evenly">
           <button
-            className="w-full bg-green-600 rounded-t-xl"
+            className="w-full bg-green-600 rounded-xl active:scale-90 ease-in-out duration-200"
             onClick={() => {
               setIsEditing(false);
             }}
@@ -288,203 +285,225 @@ export default function Lists({ list, sounds }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 flex flex-col text-white">
-        <Navbar></Navbar>
-        <article className="p-2 sm:flex sm:justify-between">
-          <h1 className="text-2xl">你的聲音列表</h1>
+      <main className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800  ">
+        <div className="sm:max-w-[600px] text-white mx-auto">
+          <Navbar></Navbar>
+          <article className="p-2 sm:flex sm:justify-between">
+            <h1 className="text-2xl">你的聲音列表</h1>
 
-          {/* CREATE NEW LIST BUTTON */}
-          <button
-            onClick={() => {
-              setIsCreating(true);
-            }}
-            className="absolute bottom-0 left-0 w-full bg-green-600 h-12 rounded-t-xl active:bg-green-900 sm:relative sm:max-w-fit sm:rounded-md sm:active:scale-90 ease-in-out duration-200"
-          >
-            Create New
-          </button>
-        </article>
-        <Transition
-          show={isEditing}
-          enter="duration-500"
-          enterFrom="top-0 opacity-0"
-          enterTo="bottom-0 opacity-100"
-          leave="duration-300"
-          leaveFrom="bottom-0 opacity-100"
-          leaveTo="top-0 opacity-0"
-        >
-          <EditListModal localListElementRefs={localListElementRefs} selectedList={selectedList} localList={localList} setLocalList={setLocalList} setIsEditing={setIsEditing}/>
-        </Transition>
-
-        {/* DIALOG TO SET NAME OF LIST */}
-
-        <ul className="p-2 flex flex-col gap-2">
-          {/* FREE LIST FROM DB FOR EVERYONE (HAOHAO'S BIRTHDAY LIST) */}
-          <li className="flex justify-between">
-            <Link
-              href={list.url}
-              className="bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
+            {/* CREATE NEW LIST BUTTON */}
+            <button
+              onClick={() => {
+                setIsCreating(true);
+              }}
+              className="absolute  px-4 bottom-0 left-0 w-full bg-green-600 h-12 rounded-t-xl active:bg-green-900 sm:relative sm:max-w-fit sm:rounded-md sm:active:scale-90 ease-in-out duration-200"
             >
-              <span>{list.name}</span>
-            </Link>
-          </li>
-          {/* LOCAL LIST IF YOU CHOOSE TO CREATE ONE */}
-          {localList ? (
+              Create New
+            </button>
+          </article>
+
+          {/* EDIT DIALOG MODAL */}
+          <Transition
+            show={isEditing}
+            enter="transition duration-500"
+            enterFrom="opacity-70"
+            enterTo="opacity-100"
+            leave="transition duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-70"
+          >
+            <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+            <Dialog 
+              onClose={() => {setIsEditing(false)}}
+              className={"fixed inset-0 flex items-center justify-center p-4"}
+            >
+              <Dialog.Panel
+              className="sm:max-w-[600px] w-full">
+                
+                <EditListModal 
+                  localListElementRefs={localListElementRefs} 
+                  selectedList={selectedList} 
+                  localList={localList} 
+                  setLocalList={setLocalList} 
+                  setIsEditing={setIsEditing}
+                />
+              </Dialog.Panel>
+            </Dialog>
+          </Transition>
+
+          {/* DIALOG TO SET NAME OF LIST */}
+
+          <ul className="p-2 flex flex-col gap-2">
+            {/* FREE LIST FROM DB FOR EVERYONE (HAOHAO'S BIRTHDAY LIST) */}
             <li className="flex justify-between">
               <Link
-                href={"freelist"}
-                className="bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md flex justify-between"
+                href={list.url}
+                className="bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
               >
-                {localList.name}
+                <span>{list.name}</span>
               </Link>
-              <button
-                className='flex items-center'
-                onClick={() => {
-                  // console.log("editing");
-                  setIsEditing(true);
-                  setSelectedList(
-                    JSON.parse(localStorage.getItem("nonUserList")).name
-                  );
-                }}
-              >
-                <span className="material-symbols-outlined">
-                  edit
-                  </span>
-              </button>
-              <button
-                className='flex items-center'
-                onClick={() => {
-                  setIsDeleting(true);
-                  return;
-                }}
-              >
-                <span className="material-symbols-outlined">
-                  delete
-                </span>
-              </button>
             </li>
-          ) : null}
-          {/* YOUR PERSONAL LISTS IF REGISTERED WILL GO HERE */}
-          {/* NO CODE FOR THIS YET */}
-          {/* MAKE USER REGISTRATION FIRST */}
-        </ul>
+            {/* LOCAL LIST IF YOU CHOOSE TO CREATE ONE */}
+            {localList ? (
+              <li className="flex justify-between">
+                <Link
+                  href={"freelist"}
+                  className="bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md flex justify-between"
+                >
+                  {localList.name}
+                </Link>
+                <button
+                  className='flex items-center'
+                  onClick={() => {
+                    // console.log("editing");
+                    setIsEditing(true);
+                    setSelectedList(
+                      JSON.parse(localStorage.getItem("nonUserList")).name
+                    );
+                  }}
+                >
+                  <span className="material-symbols-outlined">
+                    edit
+                    </span>
+                </button>
+                <button
+                  className='flex items-center'
+                  onClick={() => {
+                    setIsDeleting(true);
+                    return;
+                  }}
+                >
+                  <span className="material-symbols-outlined">
+                    delete
+                  </span>
+                </button>
+              </li>
+            ) : null}
+            {/* YOUR PERSONAL LISTS IF REGISTERED WILL GO HERE */}
+            {/* NO CODE FOR THIS YET */}
+            {/* MAKE USER REGISTRATION FIRST */}
+          </ul>
 
-        {/* DELETE MODAL */}
-        <Transition appear show={isDeleting}>
-          <Dialog
-            // open={isDeleting}
-            onClose={() => setIsDeleting(false)}
-            className={"fixed inset-0 flex items-center justify-center p-4"}
-          >
-            <Dialog.Panel>
-              <div className="bg-slate-700 p-4 rounded text-white">
-                <Dialog.Title className="mb-4">
-                  Delete List:{" "}
-                  <span className="text-2xl">{localList?.name}</span>
-                </Dialog.Title>
-                <Dialog.Description className="mb-8">
-                  You are about to permanently delete the list. Are you sure you
-                  want to do this?
-                </Dialog.Description>
-                <div className="flex justify-center gap-4">
-                  <button
-                    className="text-gray-100 px-4 py-2 rounded-xl"
-                    onClick={() => {
-                      setIsDeleting(false);
-                    }}
-                  >
-                    Cancel
-                  </button>
+          {/* DELETE MODAL */}
+          <Transition appear show={isDeleting}>
+            <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+            <Dialog
+              // open={isDeleting}
+              onClose={() => setIsDeleting(false)}
+              className={"fixed inset-0 flex items-center justify-center p-4"}
+            >
+              <Dialog.Panel>
+                <div className="bg-slate-700 p-4 rounded text-white">
+                  <Dialog.Title className="mb-4">
+                    Delete List:{" "}
+                    <span className="text-2xl">{localList?.name}</span>
+                  </Dialog.Title>
+                  <Dialog.Description className="mb-8">
+                    You are about to permanently delete the list. Are you sure you
+                    want to do this?
+                  </Dialog.Description>
+                  <div className="flex justify-center gap-4">
+                    <button
+                      className="text-gray-100 px-4 py-2 rounded-xl"
+                      onClick={() => {
+                        setIsDeleting(false);
+                      }}
+                    >
+                      Cancel
+                    </button>
 
-                  <button
-                    className="text-red-500 px-4 py-2 rounded-xl underline underline-offset-2 decoration-2"
-                    onClick={() => {
-                      localStorage.removeItem("nonUserList");
-                      setLocalList(undefined);
-                      setIsDeleting(false);
-                    }}
-                  >
-                    Delete
-                  </button>
+                    <button
+                      className="text-red-500 px-4 py-2 rounded-xl underline underline-offset-2 decoration-2"
+                      onClick={() => {
+                        localStorage.removeItem("nonUserList");
+                        setLocalList(undefined);
+                        setIsDeleting(false);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </Dialog.Panel>
-          </Dialog>
-        </Transition>
+              </Dialog.Panel>
+            </Dialog>
+          </Transition>
 
-        {/* CREATE MODAL */}
-        <Transition appear show={isCreating}>
-          <Dialog
-            // open={isDeleting}
-            onClose={() => setIsCreating(false)}
-            className={"fixed inset-0 flex items-center justify-center p-4"}
-          >
-            <Dialog.Panel>
-              <div className="bg-slate-700 p-4 rounded text-white">
-                <Dialog.Title className="mb-4">Create Sound List</Dialog.Title>
-                {localList ? (
-                  <>
-                    <Dialog.Description className="mb-8">
-                      You already have one. Please delete it to make another
-                    </Dialog.Description>
-                    <div className="flex justify-center">
-                      <button
-                        className="text-gray-100 px-4 py-2 rounded-xl"
-                        onClick={() => {
-                          setIsCreating(false);
-                        }}
-                      >
-                        Ok
-                      </button>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className="mb-8">
-                      <label>Please give your list a name</label>
-                      <input
-                        className="text-black w-full p-2 rounded mt-2"
-                        onChange={(e) => setNewListName(e.target.value)}
-                      />
-                    </div>
-                    <div className="flex justify-center gap-4">
-                      <button
-                        className="text-gray-100 px-4 py-2 rounded-xl"
-                        onClick={() => {
-                          setIsCreating(false);
-                          setNewListName("");
-                        }}
-                      >
-                        Cancel
-                      </button>
+          {/* CREATE MODAL */}
+          <Transition appear show={isCreating}>
+            <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+            <Dialog
+              // open={isDeleting}
+              onClose={() => setIsCreating(false)}
+              className={"fixed inset-0 flex items-center justify-center p-4"}
+            >
+              <Dialog.Panel>
+                <div className="bg-slate-700 p-4 rounded text-white">
+                  <Dialog.Title className="mb-4">Create Sound List</Dialog.Title>
+                  {localList ? (
+                    <>
+                      <Dialog.Description className="mb-8">
+                        You already have one. Please delete it to make another
+                      </Dialog.Description>
+                      <div className="flex justify-center">
+                        <button
+                          className="text-gray-100 px-4 py-2 rounded-xl"
+                          onClick={() => {
+                            setIsCreating(false);
+                          }}
+                        >
+                          Ok
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="mb-8">
+                        <label>Please give your list a name</label>
+                        <input
+                          className="text-black w-full p-2 rounded mt-2"
+                          onChange={(e) => setNewListName(e.target.value)}
+                        />
+                      </div>
+                      <div className="flex justify-center gap-4">
+                        <button
+                          className="text-gray-100 px-4 py-2 rounded-xl"
+                          onClick={() => {
+                            setIsCreating(false);
+                            setNewListName("");
+                          }}
+                        >
+                          Cancel
+                        </button>
 
-                      <button
-                        disabled={newListName.length < 1}
-                        className="bg-blue-500 disabled:bg-gray-500 px-4 py-2 rounded-xl"
-                        onClick={() => {
-                          localStorage.setItem(
-                            "nonUserList",
-                            JSON.stringify({
-                              name: newListName,
-                              sounds: [],
-                            })
-                          );
+                        <button
+                          disabled={newListName.length < 1}
+                          className="bg-blue-500 disabled:bg-gray-500 px-4 py-2 rounded-xl"
+                          onClick={() => {
+                            localStorage.setItem(
+                              "nonUserList",
+                              JSON.stringify({
+                                name: newListName,
+                                sounds: [],
+                              })
+                            );
 
-                          setLocalList(
-                            JSON.parse(localStorage.getItem("nonUserList"))
-                          );
-                          setNewListName("");
-                          setIsCreating(false);
-                        }}
-                      >
-                        Confirm
-                      </button>
-                    </div>
-                  </>
-                )}
-              </div>
-            </Dialog.Panel>
-          </Dialog>
-        </Transition>
+                            setLocalList(
+                              JSON.parse(localStorage.getItem("nonUserList"))
+                            );
+                            setNewListName("");
+                            setIsCreating(false);
+                          }}
+                        >
+                          Confirm
+                        </button>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </Dialog.Panel>
+            </Dialog>
+          </Transition>
+        </div>
       </main>
     </>
   );
