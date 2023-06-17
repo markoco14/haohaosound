289225aa3450d89ef6ac200dafb2e3b8666daa9d
Kos Huang <koshuang@gmail.com>
2023-06-17 13:43:44 +0800

    refactor(page): [SelectedList] use ListDetails to replace original logic

--- src/modules/sound-mgmt/infrastructure/ui/pages/SelectedList.tsx
+++ src/modules/sound-mgmt/infrastructure/ui/pages/SelectedList.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import React, { FC, useRef } from 'react';
+import { FC } from 'react';
 import { List } from '../../../domain/entities/List';
-import { Sound } from '../../../domain/entities/Sound';
 import { listAdapter } from '../../adapters/listAdapter';
-import { useList } from '../hooks/useList';
+import { ListDetails } from '../components/ListDetails';
 
 interface Props {
   list: List;
@@ -37,15 +35,6 @@ export async function getServerSideProps(context) {
 }
 
 export const SelectedList: FC<Props> = ({ list }) => {
-  console.log(list);
-
-  const { sounds, listName } = useList(list);
-
-  const elementRefs = useRef([]);
-  sounds?.forEach((_, index) => {
-    elementRefs.current[index] = React.createRef();
-  });
-
   return (
     <div>
       <Head>
@@ -57,49 +46,7 @@ export const SelectedList: FC<Props> = ({ list }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section>
-        <article className="p-2 mb-4">
-          <h1 className="text-2xl">{listName}</h1>
-        </article>
-        {sounds.length > 0 ? (
-          <ul className="p-2 flex flex-col gap-4">
-            {sounds?.map((sound: Sound, index: number) => (
-              <li key={index}>
-                <button
-                  className="w-full bg-rose-500 p-4 active:scale-95 active:bg-rose-900 active:rounded-md ease-in-out duration-200 hover:bg-rose-700 rounded-md"
-                  onClick={() => {
-                    elementRefs.current.forEach((ref, refIndex) => {
-                      if (refIndex !== index) {
-                        elementRefs.current[refIndex].current.pause();
-                        elementRefs.current[refIndex].current.currentTime = 0;
-                      }
-                    });
-                    if (!elementRefs.current[index].current.paused) {
-                      elementRefs.current[index].current.currentTime = 0;
-                    }
-                    elementRefs.current[index].current.play();
-                  }}
-                >
-                  {sound.name}
-                </button>
-                <audio ref={elementRefs.current[index]} src={sound.audio_url}>
-                  Your browser does not support the <code>audio</code> element.
-                </audio>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <article className="p-2">
-            <div className="p-8 bg-gradient-to-r from-slate-700 to-slate-800 rounded-xl">
-              <p className="mb-4">此列表中沒有聲音</p>
-              <Link
-                href="/lists"
-                className="underline decoration-2 underline-offset-8"
-              >
-                加聲音
-              </Link>
-            </div>
-          </article>
-        )}
+        <ListDetails list={list} />
       </section>
     </div>
   );
