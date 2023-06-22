9a231cc09a4830d41401108990b8aff1b61b2ab4
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-22 15:47:05 +0800

    feature: add page error handling for created list is empty

--- src/modules/sound-mgmt/infrastructure/ui/components/ListDetails.tsx
+++ src/modules/sound-mgmt/infrastructure/ui/components/ListDetails.tsx
@@ -1,8 +1,9 @@
-import { FC, createRef, useRef } from 'react';
-import { List } from '../../../domain/entities/List';
-import { Sound } from '../../../domain/entities/Sound';
-import { playback } from '../helpers/playback';
-import { SoundButton } from './SoundButton';
+import { FC, createRef, useRef } from "react";
+import { List } from "../../../domain/entities/List";
+import { Sound } from "../../../domain/entities/Sound";
+import { playback } from "../helpers/playback";
+import { SoundButton } from "./SoundButton";
+import Link from "next/link";
 
 interface Props {
   list: List;
@@ -11,9 +12,9 @@ interface Props {
 export const ListDetails: FC<Props> = ({ list }) => {
   const elementRefs = useRef([]);
 
-  const refs = list.sounds.map(() => createRef());
+  const refs = list.sounds?.map(() => createRef());
 
-  list.sounds.forEach((_, index) => {
+  list.sounds?.forEach((_, index) => {
     elementRefs.current[index] = refs[index];
   });
 
@@ -23,19 +24,26 @@ export const ListDetails: FC<Props> = ({ list }) => {
 
   return (
     <>
-      <article className="p-2">
+      <article className='p-2'>
         <h1 className="text-2xl">{list.name}</h1>
       </article>
       <ul className="p-2 flex flex-col gap-4">
-        {list.sounds.map((sound: Sound, index) => (
-          <li key={index}>
-            <SoundButton
-              ref={refs[index]}
-              sound={sound}
-              playback={() => onClick(index)}
-            />
-          </li>
-        ))}
+        {(list.sounds.length >= 1) ? (
+          list.sounds?.map((sound: Sound, index) => (
+            <li key={index}>
+              <SoundButton
+                ref={refs[index]}
+                sound={sound}
+                playback={() => onClick(index)}
+              />
+            </li>
+          ))
+        ) : (
+          <article className="bg-gray-700 rounded p-2 flex flex-col gap-4 justify-center">
+            <p>This list is empty</p>
+            <Link href="/lists">Back to lists.</Link>
+          </article>
+        )}
       </ul>
     </>
   );
